1ac191ab3e9f587672bd26c152226021607ebdcb
mh-shovon <shovon33266@gmail.com>
2023-11-20 17:37:10 +0600

    Practice Java Day-1

---
+++ src/Output.java
@@ -0,0 +1,15 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Print a Text--->");
+        System.out.println("Hello Java");
+        System.out.println("My name is Shovon\n");
+
+        System.out.println("Print a Number--->");
+        System.out.println(3);
+        System.out.println(10);
+        System.out.println(100);
+        System.out.println(1000);
+        System.out.println(10+20);
+        System.out.println(5*6);
+    }
+}
---
+++ src/Variables_Datatypes.java
@@ -0,0 +1,23 @@
+public class Variables_Datatypes {
+    public static void main(String[] args) {
+        String firstName="Mehedi";
+        String lastName="Hasan";
+        System.out.println("Name: " +firstName+" "+lastName);
+
+        int intNum1 = 10;
+        int intNum2 = 20;
+        int intSum;
+        intSum= intNum1+intNum2;
+        System.out.println("Sum is: " + intSum);
+        
+        double doubleNum1 = 20.50;
+        double doubleNum2 = 20.50;
+        System.out.println("Sum is: " +(doubleNum1+doubleNum2));
+
+        float floatNum1 = 24.5f;
+        float floatNum2 = 25.5f;
+        float floatSum = 0.0f;
+        floatSum = floatNum1+floatNum2;
+        System.out.println("Sum is: " +floatSum);
+    }
+}
