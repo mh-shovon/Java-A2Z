60ad2c6e67d6c7b4d4e55fc1691270e2734292aa
mh-shovon <shovon33266@gmail.com>
2023-11-21 15:02:21 +0600

    Practice Java Day-2

---
+++ src/Operators.java
@@ -0,0 +1,26 @@
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic Operators:--->\n");
+        int intNum1 = 20;
+        int intNum2 = 10;
+        int sum = intNum1 + intNum2;
+        System.out.println("Sum is: "+sum+"\n");
+        int sub = intNum1 - intNum2;
+        System.out.println("Sub is: "+sub+"\n");
+        int mul = intNum1 * intNum2;
+        System.out.println("Mul is: "+mul+"\n");
+        int div = intNum1 / intNum2;
+        System.out.println("Div is: "+div+"\n");
+        int modulus = intNum1 % intNum2;
+        System.out.println("Modulas is: "+modulus+"\n");
+        int x =1;
+        x++;
+        System.out.println("X is:"+x+"\n");
+        int y =5;
+        y--;
+        System.out.println("Y is:"+y+"\n");
+
+        System.out.println("Assignment Operators:--->\n");
+
+    }
+}
