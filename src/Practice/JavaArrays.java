8ed558fcbc11122389016ce8bcf3e859eb608ccc
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2023-11-24 18:47:59 +0600

    Practice Java Day-5

--- src/Practice/JavaArrays.java
+++ src/Practice/JavaArrays.java
@@ -18,14 +18,31 @@ public class JavaArrays {
         System.out.println("\n" + "For Loop Through an Array--->");
         String[] cars1 = { "Volvo", "BMW", "Ford", "Mazda" };
         for (int i = 0; i < cars1.length; i++) {
-            System.out.println("Elements of the array are: "+cars1[i]);
+            System.out.println("Elements of the array are: " + cars1[i]);
         }
         System.out.println("\n" + "For Each Loop Through an Array--->");
-        String [] cars3 = { "Volvo", "BMW", "Ford", "Mazda" };
-        for (String j : cars3) {
-            System.out.println("Elements of the array are: "+j);           
+        String[] cars2 = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (String j : cars2) {
+            System.out.println("Elements of the array are: " + j);
         }
 
+        System.out.println("\n" + "Multidimensional Arrays--->");
+        int[][] myNum1 = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+        System.out.println(myNum1[0][3]); // Access Eement of these index.
+        System.out.println(myNum1[1][2]); // Access Eement of these index.
 
+        System.out.println("\n" + "Change Values of an Element--->");
+        int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+        myNumbers[1][2] = 8;
+        System.out.println(myNumbers[1][2]); // Outputs 8 instead of 7
+
+        System.out.println("\n" + "Loop Through a Multi-Dimensional Array--->");
+        int[][] myNumbers3 = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+        System.out.println("Print the length of the array: "+myNumbers3.length);
+        for (int i = 0; i < myNumbers3.length; ++i) {
+            for (int j = 0; j < myNumbers3[i].length; ++j) {
+                System.out.println(myNumbers3[i][j]);
+            }
+        }
     }
 }
